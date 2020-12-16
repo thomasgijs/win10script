b1eca296ec693cb47e03beae2fb8013481451524
thomasgijs <36015515+thomasgijs@users.noreply.github.com>
2020-12-16 11:29:46 +0100

    tcui instead of xboxapp

--- w10debloaterOnly.ps1
+++ w10debloaterOnly.ps1
@@ -38,11 +38,11 @@ $Bloatware = @(
         "Microsoft.WindowsFeedbackHub"
         <# "Microsoft.WindowsMaps" #>
         "Microsoft.WindowsSoundRecorder"
-	"Microsoft.Xbox.TCUI"
-	<# "Microsoft.XboxApp" ">
+	<# "Microsoft.Xbox.TCUI" #>
 	<# wasn't able to remove this (error 0x80073CFA). 
 	Possible solution is to go into unrestricted mode and put developper mode on. 
 	Not comfortable with this. Still most xbox features are removed#>
+	"Microsoft.XboxApp"
 	"Microsoft.XboxGameCallableUI"
 	"Microsoft.XboxGameOverlay"
 	"Microsoft.XboxGamingOverlay"
