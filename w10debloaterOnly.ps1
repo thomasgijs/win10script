f87248e344b7ba4f84c526aed9e7fde2f22385da
thomasgijs <36015515+thomasgijs@users.noreply.github.com>
2020-12-16 08:41:47 +0100

    deleted xbox try 3 :)

--- w10debloaterOnly.ps1
+++ w10debloaterOnly.ps1
@@ -12,13 +12,13 @@ $Bloatware = @(
         #Unnecessary Windows 10 AppX Apps
         "Microsoft.3DBuilder"
         "Microsoft.AppConnector"
-	    "Microsoft.BingFinance"
-	    "Microsoft.BingNews"
-	    "Microsoft.BingSports"
-	    "Microsoft.BingTranslator"
-	    "Microsoft.BingWeather"
-        "Microsoft.GetHelp"
-        "Microsoft.Getstarted"
+	"Microsoft.BingFinance"
+	"Microsoft.BingNews"
+	"Microsoft.BingSports"
+	"Microsoft.BingTranslator"
+	"Microsoft.BingWeather"
+        <# "Microsoft.GetHelp" #>
+        <# "Microsoft.Getstarted" #>
         <# "Microsoft.Messaging" #>
         "Microsoft.Microsoft3DViewer"
         "Microsoft.MicrosoftSolitaireCollection"
@@ -27,7 +27,7 @@ $Bloatware = @(
         "Microsoft.Office.Lens"
         "Microsoft.Office.Sway"
         "Microsoft.OneConnect"
-        "Microsoft.People"
+        <# "Microsoft.People" #>
         "Microsoft.Print3D"
         "Microsoft.SkypeApp"
         "Microsoft.StorePurchaseApp"
@@ -36,8 +36,16 @@ $Bloatware = @(
         "Microsoft.WindowsAlarms"
         <# "microsoft.windowscommunicationsapps" #>
         "Microsoft.WindowsFeedbackHub"
-        "Microsoft.WindowsMaps"
+        <# "Microsoft.WindowsMaps" #>
         "Microsoft.WindowsSoundRecorder"
+	"Microsoft.Xbox.TCUI"
+	"Microsoft.XboxApp"
+	"Microsoft.XboxGameCallableUI"
+	"Microsoft.XboxGameOverlay"
+	"Microsoft.XboxGamingOverlay"
+	"Microsoft.XboxIdentityProvider"
+	"Microsoft.XboxLive"
+	"Microsoft.XboxSpeechToTextOverlay"
         "Microsoft.ZuneMusic"
         "Microsoft.ZuneVideo"
 
@@ -67,7 +75,6 @@ $Bloatware = @(
         "*Hulu*"
         "*HiddenCity*"
         "*AdobePhotoshopExpress*"
-	"*XboxGameBar*"
                      
         #Optional: Typically not removed but you can if you need to for some reason
         #"*Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe*"
