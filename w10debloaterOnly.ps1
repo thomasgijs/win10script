af5c3ffff33165ce4aeedae00eb42c375e13745f
thomasgijs <36015515+thomasgijs@users.noreply.github.com>
2020-12-16 11:33:12 +0100

    <# "Microsoft.XboxApp" #>

--- w10debloaterOnly.ps1
+++ w10debloaterOnly.ps1
@@ -42,7 +42,7 @@ $Bloatware = @(
 	<# wasn't able to remove this (error 0x80073CFA). 
 	Possible solution is to go into unrestricted mode and put developper mode on. 
 	Not comfortable with this. Still most xbox features are removed#>
-	"Microsoft.XboxApp"
+	<# "Microsoft.XboxApp" #>
 	"Microsoft.XboxGameCallableUI"
 	"Microsoft.XboxGameOverlay"
 	"Microsoft.XboxGamingOverlay"
